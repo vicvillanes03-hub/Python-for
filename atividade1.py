24e3afa3ee1b0fc41af74daf7d9c23afa14de5ed
Victoria de Moraes Villanes <victoria.villanes@portalsesisp.org.br>
2026-06-11 14:25:01 -0300

    primeiro

---
+++ atividade1.py
@@ -0,0 +1,42 @@
+vendedores = int(input("Informe quantos vendedores fazem parte da equipe: "))
+dias = int(input("Informe quantos dias: "))
+equipe = 0.0
+melhorVendedor = ""
+maiorVenda = 0.0
+vendaDia = 0.0
+
+for vendedor in range(vendedores):
+    nome = input("\nDigite o nome do vendedor: ")
+    total = 0.0
+    folgas = 0
+
+    for dia in range(dias):
+        venda = float(input(f"Digite o valor vendido no dia {dia + 1}: "))
+        
+        if venda < 0:
+            continue 
+        
+        elif venda == 0:
+            folgas += 1
+            continue
+
+        total += venda 
+        equipe += venda
+
+        if venda > vendaDia:
+            vendaDia = venda
+        
+    print("Valor total vendido por",nome +" : R$",total)
+    
+    dias_trabalhados = dias - folgas
+    if dias_trabalhados > 0:
+        media = total / dias_trabalhados
+        print("Média diária de vendas: R$", media)
+
+    if total > maiorVenda:
+        maiorVenda = total 
+        melhorVendedor = nome
+
+print("Total vendido pela equipe: R$", equipe)
+print("O melhor vendedor foi: ", melhorVendedor)
+print("O maior valor de venda registrado em um único dia foi: R$", vendaDia)
